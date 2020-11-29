16f71c5b9c9b525eb90bcc1dba84af0aa1922881
joaquin <jdevicente@genasys.com>
2020-11-29 17:32:02 +0100

    First commit

---
+++ src/main/java/usantatecla/tictactoe/views/console/StartView.java
@@ -0,0 +1,26 @@
+package usantatecla.tictactoe.views.console;
+
+import usantatecla.tictactoe.controllers.StartController;
+import usantatecla.tictactoe.views.Message;
+import usantatecla.utils.Console;
+import usantatecla.utils.LimitedIntDialog;
+
+class StartView {
+
+	private StartController startController;
+
+	StartView(StartController startController) {
+		assert startController != null;
+		
+		this.startController = startController;
+	}
+
+	void interact() {
+		Console.getInstance().writeln(Message.TITTLE.toString());
+		int users = new LimitedIntDialog(0, 
+			this.startController.getMaxPlayers()).read(Message.NUMBER_PLAYERS.toString());
+		this.startController.setUsers(users);
+		new GameView(startController).write();
+	}
+
+}
